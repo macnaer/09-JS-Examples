84671328a2a1845ec35ef2081fadc36023da8033
master <master@rivne.itstep.org>
2021-05-19 10:54:43 +0300

    example1

---
+++ example1/js/common.js
@@ -0,0 +1,34 @@
+let btnSave = document.getElementById("btnSave");
+btnSave.addEventListener('click', GetPerson);
+let name = document.getElementById("Name");
+let surname = document.getElementById("Surname");
+let age = document.getElementById("Age");
+
+let Persons = [];
+
+function GetPerson(event) {
+    event.preventDefault();
+    // console.log(`Name: ${name.value}\nSurname: ${surname.value}\nAge: ${age.value}`)
+    let Person = {
+        name: name.value,
+        surname: surname.value,
+        age: age.value,
+        Print() {
+            console.log(`${this.name}\n${this.surname}\n${this.age}`)
+        }
+    }
+    Persons.push(Person);
+
+    name.value = ""
+    surname.value = ""
+    age.value = ""
+
+    Persons.forEach(person => {
+        person.Print();
+    })
+
+    console.log(Persons.length)
+}
+
+
+
